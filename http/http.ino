d1e4e76740248aa5ba0cf646a8bbda971f9392db
Rate980 <xv0nnj0@icloud.com>
2023-03-13 23:54:06 +0900

    Add Json

--- http/http.ino
+++ http/http.ino
@@ -1,9 +1,4 @@
-/**
- * BasicHTTPClient.ino
- *
- *  Created on: 24.05.2015
- *
- */
+#include <ArduinoJson.h>
 
 #include <Arduino.h>
 
@@ -11,93 +6,96 @@
 #include <WiFiMulti.h>
 
 #include <HTTPClient.h>
+#include <string>
+#include <ArduinoJson.h>
 
 #define USE_SERIAL Serial
+#define SW1 14
 
 WiFiMulti wifiMulti;
 
-/*
-const char* ca = \ 
-"-----BEGIN CERTIFICATE-----\n" \  
-"MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \  
-"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \  
-"DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \  
-"SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \  
-"GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \  
-"AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \  
-"q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \  
-"SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \  
-"Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \  
-"a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \  
-"/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \  
-"AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \  
-"CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \  
-"bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \  
-"c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \  
-"VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \  
-"ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \  
-"MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \  
-"Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \  
-"AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \  
-"uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \  
-"wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \  
-"X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \  
-"PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \  
-"KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \  
-"-----END CERTIFICATE-----\n";
-*/
+const char *url_base = "https://nekoze-9ccfb-default-rtdb.asia-southeast1.firebasedatabase.app/test";
+const char *url_suffix = ".json";
+const char *timeStampData = R"({".sv":"timestamp"})";
 
-const char* url = "https://nekoze-9ccfb-default-rtdb.asia-southeast1.firebasedatabase.app/.json";
+boolean swDataOld = false;
+boolean swData;
 
-void setup() {
+void setup()
+{
 
     USE_SERIAL.begin(115200);
+    wifiMulti.addAP("maruyama", "marufuck");
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+    pinMode(swData, INPUT);
 
-    for(uint8_t t = 4; t > 0; t--) {
+    for (uint8_t t = 4; t > 0; t--)
+    {
         USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
         USE_SERIAL.flush();
         delay(1000);
     }
-
-    wifiMulti.addAP("maruyama", "marufuck");
-
 }
 
-void loop() {
+void loop()
+{
     // wait for WiFi connection
-    if((wifiMulti.run() == WL_CONNECTED)) {
-
-        HTTPClient http;
-
-//        USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-        http.begin(url); //HTTP
-
-//        USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
-
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("%d\n", httpCode);
-
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
+    swData = digitalRead(SW1);
+    if ((wifiMulti.run() != WL_CONNECTED))
+    {
+        return;
     }
 
-    delay(5000);
+    if (swData != swDataOld && swData)
+    {
+        HTTPClient http;
+        String url = String(url_base);
+        // url += url_suffix;
+        http.begin(url + url_suffix);
+        http.setReuse(true);
+        http.addHeader("Content-Type", "application/json");
+        auto resCode = http.POST(R"({"state":3})");
+        if (resCode < 0)
+        {
+            Serial.printf("[error] %s", http.errorToString(resCode).c_str());
+            http.end();
+        }
+        else
+        {
+        }
+    }
+
+    // HTTPClient http;
+
+    //        USE_SERIAL.print("[HTTP] begin...\n");
+    // configure traged server and url
+    // http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+    // http.begin(url); // HTTP
+
+    //        USE_SERIAL.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    // int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    // if (httpCode > 0)
+    // {
+    //     // HTTP header has been send and Server response header has been handled
+    //     USE_SERIAL.printf("%d\n", httpCode);
+
+    //     // file found at server
+    //     if (httpCode == HTTP_CODE_OK)
+    //     {
+    //         String payload = http.getString();
+    //         USE_SERIAL.println(payload);
+    //     }
+    // }
+    // else
+    // {
+    //     USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    // }
+
+    // http.end();
+
+    delay(10);
+    swDataOld = swData;
 }
