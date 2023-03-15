e3c005270e16e225535c3e15e8e146e646e24ef3
Rate980 <ratei.5673@gmail.com>
2023-03-15 18:20:02 +0900

    style auto format

--- NekozeB/NekozeB.ino
+++ NekozeB/NekozeB.ino
@@ -1,4 +1,3 @@
-
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
@@ -7,27 +6,31 @@ int DC_PIN = 5;
 
 Adafruit_MPU6050 mpu;
 
-void setup(void) {
-  pinMode(DC_PIN,OUTPUT);
+void setup(void)
+{
+  pinMode(DC_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
-  
+
   Serial.println("Adafruit MPU6050 test!");
 
   // Try to initialize!
-  if (!mpu.begin()) {
+  if (!mpu.begin())
+  {
     Serial.println("Failed to find MPU6050 chip");
-    while (1) {
+    while (1)
+    {
       delay(10);
     }
   }
   Serial.println("MPU6050 Found!");
-  
+
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
   Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
+  switch (mpu.getAccelerometerRange())
+  {
   case MPU6050_RANGE_2_G:
     Serial.println("+-2G");
     break;
@@ -43,7 +46,8 @@ void setup(void) {
   }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
+  switch (mpu.getGyroRange())
+  {
   case MPU6050_RANGE_250_DEG:
     Serial.println("+- 250 deg/s");
     break;
@@ -57,10 +61,11 @@ void setup(void) {
     Serial.println("+- 2000 deg/s");
     break;
   }
-  
+
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
   Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
+  switch (mpu.getFilterBandwidth())
+  {
   case MPU6050_BAND_260_HZ:
     Serial.println("260 Hz");
     break;
@@ -83,16 +88,17 @@ void setup(void) {
     Serial.println("5 Hz");
     break;
   }
-  
+
   Serial.println("");
   delay(100);
 }
-  
-void loop() {
+
+void loop()
+{
   /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  
+
   /* Print out the values */
   Serial.print("Acceleration X: ");
   Serial.print(a.acceleration.x);
@@ -101,7 +107,7 @@ void loop() {
   Serial.print(", Z: ");
   Serial.print(a.acceleration.z);
   Serial.println(" m/s^2");
-  
+
   Serial.print("Rotation X: ");
   Serial.print(g.gyro.x);
   Serial.print(", Y: ");
@@ -109,8 +115,7 @@ void loop() {
   Serial.print(", Z: ");
   Serial.print(g.gyro.z);
   Serial.println(" rad/s");
-  
-  
+
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
   Serial.println(" degC");
@@ -118,28 +123,35 @@ void loop() {
   Serial.println("");
   Serial.println("");
 
-  if(a.acceleration.x <= 2){
+  if (a.acceleration.x <= 2)
+  {
     Serial.println("正常だにゃ");
-    digitalWrite(DC_PIN,LOW);
+    digitalWrite(DC_PIN, LOW);
     Serial.println("");
     Serial.println("");
     digitalWrite(LED_PIN, LOW);
-  }else if(a.acceleration.x >= 3 && a.acceleration.x < 4){
-    Serial.println("猫背だにゃ");
-    digitalWrite(DC_PIN,LOW);
-    Serial.println("");
-    Serial.println("");
-    digitalWrite(LED_PIN, HIGH);
-  }else if(a.acceleration.x >= 4){
-    Serial.println("ゴミくずだにゃ");
-    digitalWrite(DC_PIN,HIGH);
-    Serial.println("");
-    Serial.println("");
-    digitalWrite(LED_PIN, HIGH);
-  }else{
-    digitalWrite(LED_PIN, LOW);
-    digitalWrite(DC_PIN,LOW);
   }
-  
+  else if (a.acceleration.x >= 3 && a.acceleration.x < 4)
+  {
+    Serial.println("猫背だにゃ");
+    digitalWrite(DC_PIN, LOW);
+    Serial.println("");
+    Serial.println("");
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else if (a.acceleration.x >= 4)
+  {
+    Serial.println("ゴミくずだにゃ");
+    digitalWrite(DC_PIN, HIGH);
+    Serial.println("");
+    Serial.println("");
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(DC_PIN, LOW);
+  }
+
   delay(1000);
 }
